8b54c61a0d592173edbaf0a9549ab5ff36cf4d41
Plasmoxy <plasmoxy@gmail.com>
2022-02-19 21:51:40 +0100

    ORIGINAL

---
+++ proxi/main.py
@@ -0,0 +1,13 @@
+"""
+==== SIP proxy ===
+
+1. zadanie z MTAA
+Sebastián Petrík
+19.2.2022
+MTAA
+
+SIP proxy using modified library by Philppe Thirion
+Original code: https://github.com/tirfil/PySipFullProxy/blob/master/sipfullproxy.py
+Original license: gpl.txt
+
+"""
