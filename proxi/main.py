fbb7333c5d70c97cbbcf24adf7d7379fb6e397a4
Plasmoxy <plasmoxy@gmail.com>
2022-02-28 19:09:57 +0100

    ip addr check

--- proxi/main.py
+++ proxi/main.py
@@ -21,16 +21,20 @@ if __name__ == "__main__":
     print("=== Assignment 1 from MTAA 27.2.2022 Sebastian Petrik ===")
     print("SIP proxy modified to run on python 3.9 with improvements.")
     
-    hostn, n, ips = socket.gethostbyname_ex(socket.gethostname())
-    print("Select IP for proxy (enter number): ")
-    print("0 = Enter ip manually")
-    for i in range(len(ips)):
-        print(f"{i+1} = {ips[i]}")
-    ipidx = int(input("Enter choice number: "))
-    if (ipidx == 0):
+    try:
+        hostn, n, ips = socket.gethostbyname_ex(socket.gethostname())
+        print("Select IP for proxy (enter number): ")
+        print("0 = Enter ip manually")
+        for i in range(len(ips)):
+            print(f"{i+1} = {ips[i]}")
+        ipidx = int(input("Enter choice number: "))
+        if (ipidx == 0):
+            ip = input("Enter IP: ")
+        else:
+            ip = ips[ipidx-1]
+    except:
+        print("Couldn't load ip addresses, enter IP manually.")
         ip = input("Enter IP: ")
-    else:
-        ip = ips[ipidx-1]
     
     port = 5060
     
